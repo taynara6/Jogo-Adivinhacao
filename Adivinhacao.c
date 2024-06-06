093dde90f3e66b7f605688c9e49db3e0c40a63e1
taynara6 <taynarasilvarocha6@gmail.com>
2024-06-06 19:52:49 -0300

    Adicionando variaveis

--- Adivinhacao.c
+++ Adivinhacao.c
@@ -7,5 +7,9 @@ printf("************************************\n");
 printf("* Bem-vindo ao Jogo de Adivinhação *\n");
 printf("************************************\n");
 
+int numerosecreto;
+numerosecreto = 42;
+printf("%d", 42 );
+printf("\n O numero %d e secreto!");
  return 0;
 }
