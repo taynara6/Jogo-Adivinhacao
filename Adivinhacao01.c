c8e68e0c846bbecc2a04ba81751651df6853505b
taynara6 <taynarasilvarocha6@gmail.com>
2024-06-07 18:30:33 -0300

    Melhorando a legibilidade do codigo

--- Adivinhacao01.c
+++ Adivinhacao01.c
@@ -7,14 +7,14 @@ printf("* Bem-vindo ao Jogo de Adivinhacao *\n");
 printf("************************************\n");
 
 //Lendo o teclado
-int chute, numerosecreto;
+int chute, numerosecreto, acertou;
 numerosecreto = 42;
 printf("Qual e seu chute?\n");
 scanf("%d", &chute);
 printf("Voce chutou o numero %d \n", chute);
-
+acertou = chute == numerosecreto;
 //Controle de fluxo com if
-if (chute==numerosecreto){
+/*if (chute==numerosecreto){
     printf("Parabens! Voce acertou.\n");
     printf("Jogue de novo, você é um bom jogador!\n");
 }
@@ -28,11 +28,29 @@ else {
     {
         printf("O seu chute foi menor que o numero secreto \n");
     }
-    
-    
-    printf("Mas não desanime, tente de novo!\n");
-
+    printf("Mas nao desanime, tente de novo!\n");
+}*/
+// Melhorar a legibilidade do codigo
+if (acertou)
+{
+   printf("Parabens! Voce acerto o numero secreto. \n");
 }
+else
+{
+    printf("Voce errou!\n");
+    int maior;
+    maior = chute == numerosecreto;
+    if (maior)
+    {
+       printf("Voce chutou um numero maior que o numero secreto.\n");
+    }
+    else
+    {
+       printf("Voce chutou um numero menor que o numero secreto.\n");
+    }
+    
+}
+
 
  return 0;
 }
