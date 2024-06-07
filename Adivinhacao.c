cde577cdfb124e41945545aaf60e64a42ef4f467
taynara6 <taynarasilvarocha6@gmail.com>
2024-06-07 18:03:27 -0300

    Controle de fluxo com if

--- Adivinhacao.c
+++ Adivinhacao.c
@@ -7,9 +7,11 @@ printf("************************************\n");
 printf("* Bem-vindo ao Jogo de Adivinhação *\n");
 printf("************************************\n");
 
+//Adicionando uma variavel
 int numerosecreto;
 numerosecreto = 42;
 printf("%d", 42 );
-printf("\n O numero %d e secreto!");
+printf("\n O numero %d e secreto! \n", numerosecreto);
+
  return 0;
 }
--- Adivinhacao01.c
+++ Adivinhacao01.c
@@ -7,9 +7,32 @@ printf("* Bem-vindo ao Jogo de Adivinhacao *\n");
 printf("************************************\n");
 
 //Lendo o teclado
-int chute;
-printf("Qual e seu chute?");
+int chute, numerosecreto;
+numerosecreto = 42;
+printf("Qual e seu chute?\n");
 scanf("%d", &chute);
-printf("Voce chutou o numero %d", chute);
+printf("Voce chutou o numero %d \n", chute);
+
+//Controle de fluxo com if
+if (chute==numerosecreto){
+    printf("Parabens! Voce acertou.\n");
+    printf("Jogue de novo, você é um bom jogador!\n");
+}
+else {
+    printf("Voce errou!\n");
+    if (chute>numerosecreto)
+    {
+        printf("O seu chute foi maior que o numero secreto \n");
+    }
+    if (chute<numerosecreto)
+    {
+        printf("O seu chute foi menor que o numero secreto \n");
+    }
+    
+    
+    printf("Mas não desanime, tente de novo!\n");
+
+}
+
  return 0;
 }
